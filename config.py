3b72fd2327f35bd5ea43c89b545192d5f1fbc00a
Forevka <32968153+Forevka@users.noreply.github.com>
2021-07-21 14:57:33 +0000

    up

---
+++ config.py
@@ -0,0 +1,10 @@
+from os import environ
+
+SESSION_NAME = environ.get('SESSION_NAME', 'pytgcalls')
+
+API_HASH = environ.get('API_HASH', '')
+API_ID = environ.get('API_ID', '')
+
+ADMINS = [
+    383492784,
+]
--- radio.py
+++ radio.py
@@ -1,76 +1,56 @@
 import signal
 
-import ffmpeg  # pip install ffmpeg-python
-from pyrogram import Client, filters, idle
+import ffmpeg
+from pyrogram import Client, filters
 from pyrogram.types import Message
-import asyncio
-import os
+from pytgcalls import GroupCallFactory
 
-from pytgcalls import GroupCallFactory, GroupCallFactory  # pip install pytgcalls[pyrogram]
+from config import ADMINS, API_HASH, API_ID, SESSION_NAME
 
-# Example of pinned message in a chat:
-'''
-Radio stations:
-1. https://hls-01-regions.emgsound.ru/11_msk/playlist.m3u8
-To start reply to this message with command !start <ID>
-To stop use !stop command
-'''
-
-INPUT_DEVICE_NAME = 'MacBook Air Microphone'
-OUTPUT_DEVICE_NAME = 'MacBook Air Speakers'
 CLIENT_TYPE = GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM
 
-
 pyro_client = Client(
-    'sessions/pytgcalls',
-    api_hash=os.environ.get('API_HASH', ''),
-    api_id=os.environ.get('API_ID', 0),
+    f"sessions/{SESSION_NAME}",
+    api_hash=API_HASH,
+    api_id=API_ID,
 )
 
-
 # Commands available only for anonymous admins
-async def anon_filter(_, __, m: Message):
-    return bool(m.from_user is None and m.sender_chat)
 
 
-anonymous = filters.create(anon_filter)
+async def admin_filter(_, __, m: Message):
+    return m.from_user.id in ADMINS
+
+
+admin = filters.create(admin_filter)
 
 GROUP_CALLS = {}
 FFMPEG_PROCESSES = {}
 
 
-@pyro_client.on_message(anonymous & filters.command('start', prefixes='!'))
+@pyro_client.on_message(admin & filters.command('start', prefixes='!'))
 async def start(client, message: Message):
-    input_filename = f'radio-{message.chat.id}.raw'
-
-    group_call = GROUP_CALLS.get(message.chat.id)
-    if group_call is None:
-        group_call = GroupCallFactory(client, path_to_log_file='').get_file_group_call(input_filename)
-        GROUP_CALLS[message.chat.id] = group_call
-
-    a = len(message.command) < 2
-    if not message.reply_to_message or a:
-        await message.reply_text('You forgot to replay list of stations or pass a station ID')
+    if not message.reply_to_message or len(message.command) < 3:
+        await message.reply_text('!start @group_username https://link-to-stream/')
         return
 
-    process = FFMPEG_PROCESSES.get(message.chat.id)
+    group_id = await pyro_client.get_chat(message.command[1].replace('@', ''))
+
+    input_filename = f'stations/radio-{group_id}.raw'
+
+    group_call = GROUP_CALLS.get(group_id)
+    if group_call is None:
+        group_call = GroupCallFactory(
+            client, path_to_log_file='').get_file_group_call(input_filename)
+        GROUP_CALLS[group_id] = group_call
+
+    process = FFMPEG_PROCESSES.get(group_id)
     if process:
         process.send_signal(signal.SIGTERM)
 
-    station_stream_url = None
-    station_id = message.command[1]
-    msg_lines = message.reply_to_message.text.split('\n')
-    for line in msg_lines:
-        line_prefix = f'{station_id}. '
-        if line.startswith(line_prefix):
-            station_stream_url = line.replace(line_prefix, '').replace('\n', '')
-            break
+    station_stream_url = message.command[2].strip()
 
-    if not station_stream_url:
-        await message.reply_text(f'Can\'t find a station with id {station_id}')
-        return
-
-    await group_call.start(message.chat.id)
+    await group_call.start(group_id)
 
     process = (
         ffmpeg.input(station_stream_url)
@@ -78,18 +58,24 @@ async def start(client, message: Message):
         .overwrite_output()
         .run_async()
     )
-    FFMPEG_PROCESSES[message.chat.id] = process
+    FFMPEG_PROCESSES[group_id] = process
 
-    await message.reply_text(f'Radio #{station_id} is playing...')
+    await message.reply_text(f'Radio for {group_id} with {station_stream_url} is playing...')
 
 
-@pyro_client.on_message(anonymous & filters.command('stop', prefixes='!'))
+@pyro_client.on_message(admin & filters.command('stop', prefixes='!'))
 async def stop(_, message: Message):
-    group_call = GROUP_CALLS.get(message.chat.id)
+    if not message.reply_to_message or len(message.command) < 2:
+        await message.reply_text('!stop @group_username')
+        return
+
+    group_id = await pyro_client.get_chat(message.command[1].replace('@', ''))
+
+    group_call = GROUP_CALLS.get(group_id)
     if group_call:
         await group_call.stop()
 
-    process = FFMPEG_PROCESSES.get(message.chat.id)
+    process = FFMPEG_PROCESSES.get(group_id)
     if process:
         process.send_signal(signal.SIGTERM)
 
